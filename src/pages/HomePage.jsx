85ebfbe4a97ce3665db05c8d2edf37c0512b076b
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2024-03-27 15:15:58 +0330

    feat: logout user on logout button click
    closes #8

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
 import withAuth from "../components/withAuth"
+import useUserStore from "../store/user.store"
 
 function HomePage() {
-  return <div>Home Page</div>
+  const removeUser = useUserStore(
+    (state) => state.removeUser
+  )
+
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <button type="button" onClick={removeUser}>
+        Logout
+      </button>
+    </div>
+  )
 }
 
 const ProtectedHomePage = withAuth(HomePage)
