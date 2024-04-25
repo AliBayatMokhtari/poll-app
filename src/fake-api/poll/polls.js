decd4646858fb66defe412f6610da8f1cc4c64e5
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2024-04-25 22:29:27 +0330

    chore: polls loading shimmer

--- src/fake-api/poll/polls.js
+++ src/fake-api/poll/polls.js
@@ -3,7 +3,7 @@ import delay from "../utils/delay";
 
 /** @type {Api.Poll.GetPollsService} */
 const getAll = async () => {
-	await delay();
+	await delay(1000);
 	return POLLS;
 };
 export default getAll;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,23 @@
-import { Container, Heading } from "@radix-ui/themes";
+import { Box, Container, Grid, Heading } from "@radix-ui/themes";
 import withAuth from "../components/withAuth";
 import pollService from "../services/api/poll.service";
 import { useEffect, useState } from "react";
+import Shimmer from "../components/shimmer-loading/Shimmer";
 
 function HomePage() {
 	const [polls, setPolls] = useState(/** @type {Array<Domain.Poll>} */ ([]));
+	const [loading, setLoading] = useState(false);
 
 	const getPolls = async () => {
-		const _polls = await pollService.getAllPolls();
-		setPolls(_polls);
+		setLoading(true);
+		try {
+			const _polls = await pollService.getAllPolls();
+			setPolls(_polls);
+		} catch (err) {
+			console.log({ err });
+		} finally {
+			setLoading(false);
+		}
 	};
 
 	useEffect(() => {
@@ -18,12 +27,35 @@ function HomePage() {
 	return (
 		<Container mt="5">
 			<Heading as="h2">Home Page</Heading>
-			{polls.map(poll => (
-				<div key={poll.id}>{poll.title}</div>
-			))}
+			<Box mt="4">
+				{loading ? (
+					<PollsLoadingShimmer />
+				) : polls.length ? (
+					polls.map(poll => <div key={poll.id}>{poll.id}</div>)
+				) : (
+					<EmptyView />
+				)}
+			</Box>
 		</Container>
 	);
 }
 
 const ProtectedHomePage = withAuth(HomePage);
 export default ProtectedHomePage;
+
+const PollsLoadingShimmer = () => {
+	const polls = Array(9).fill(undefined);
+	return (
+		<Grid columns="3" gapX="1" gapY="1">
+			{polls.map((_, idx) => (
+				<Shimmer key={idx}>
+					<Box height="200px" />
+				</Shimmer>
+			))}
+		</Grid>
+	);
+};
+
+const EmptyView = () => {
+	return <Box>List Empty</Box>;
+};
