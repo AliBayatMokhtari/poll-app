27639c743f47177b111874808d7f1b032d35bd2d
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2024-04-25 19:46:07 +0330

    chore: added sign up fake api

--- src/fake-api/auth/sign-up.js
+++ src/fake-api/auth/sign-up.js
@@ -1,2 +1,21 @@
-const signUp = async () => {};
+/**
+ * @type {Api.Auth.SignUpService}
+ */
+const signUp = async (payload) => {
+	return {
+		jwt: "Fake JWT",
+		user: {
+			confirmed: true,
+			blocked: false,
+			createdAt: "",
+			email: payload.email,
+			firstName: payload.firstName,
+			id: 10,
+			lastName: payload.lastName,
+			provider: "local",
+			updatedAt: "",
+			username: payload.username,
+		},
+	};
+};
 export default signUp;
