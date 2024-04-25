8f4dff7088d5da9eb1b1e3c1526407093663548b
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2024-04-25 18:33:12 +0330

    chore: used fake api for login service

--- src/services/api/auth.service.js
+++ src/services/api/auth.service.js
@@ -1,12 +1,15 @@
+import fakeApi from "../../fake-api";
 import httpService from "../core/http.service";
 
 /**
  * @type {Api.Auth.LoginService}
  */
 function login(payload) {
-	const loginPromise = httpService.post("/auth/local", {
-		...payload,
-	});
+	// const loginPromise = httpService.post("/auth/local", {
+	// 	...payload,
+	// });
+
+	const loginPromise = fakeApi.auth.login({ ...payload });
 
 	return loginPromise;
 }
